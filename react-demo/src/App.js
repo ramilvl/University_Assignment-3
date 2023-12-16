27d8f950b5bc63e751c3e214b932f45cc6acbdd0
Ramil Valiyev <ramil4360616@gmail.com>
2023-12-16 22:06:39 +0400

    bug fixes

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Testing</p>
+        
       </header>
     </div>
   );
