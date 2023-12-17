87903b148b5e59cfc9c9eb5bd0ef67d1a5eb5694
Ramil Valiyev <ramil4360616@gmail.com>
2023-12-17 22:08:57 +0400

    header part sckeleton is done

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -2,7 +2,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <span>Home Page</span>
+        <span>Flash Card</span>
+        <span>Contact me</span>
       </header>
     </div>
   );
