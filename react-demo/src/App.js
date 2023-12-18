26095d8e256a7a4f1ad42038397b3e4571c1eb4b
Ramil Valiyev <ramil4360616@gmail.com>
2023-12-18 23:06:56 +0400

    some css changes and skeleton of the project is done

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -2,9 +2,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <span>Home Page</span>
-        <span>Flash Card</span>
-        <span>Contact me</span>
+        <span>Home Page</span><br></br>
+        <span>Flash Card</span><br></br>
+        <span>Contact Page</span>
       </header>
     </div>
   );
