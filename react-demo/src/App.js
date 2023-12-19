68472c93d7319d595ffe77cffeb1a082fcd24e93
Ramil Valiyev <ramil4360616@gmail.com>
2023-12-19 23:41:34 +0400

    research note

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -5,6 +5,12 @@ function App() {
         <span>Home Page</span><br></br>
         <span>Flash Card</span><br></br>
         <span>Contact Page</span>
+
+        /*
+        Important Note: For today, I only watch recording and some youtube videos to understand the assignment.
+        I still have difficulties in understanding of how to implement ht elogic. Thus, today's commit will be only about research.
+        Thank you.
+        */
       </header>
     </div>
   );
