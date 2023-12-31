ce07a12e45c9bad17a28e0b11ad50af115dd4faf
Ramil Valiyev <ramil4360616@gmail.com>
2023-12-31 17:39:00 +0400

    changes

--- react-demo/src/Navbar.js
+++ react-demo/src/Navbar.js
@@ -1,23 +1,23 @@
-import "../src/style/home.css";
-import { Outlet, Link } from "react-router-dom";
-const Navbar=()=>{
-    return(
-      /*
-      Navigation abr to control the header.
-      */
-        <nav>
-        <input type="checkbox" id="check" />
-        <label htmlFor="check" className="checkbtn">
-          <i className="fas fa-bars" />
-        </label>
-        <label className="logo">DesignX</label>
-        <ul>
-            <Link to='/home'>Home</Link>
+// import "../src/style/home.css";
+// import { Outlet, Link } from "react-router-dom";
+// const Navbar=()=>{
+//     return(
+//       /*
+//       Navigation abr to control the header.
+//       */
+//         <nav>
+//         <input type="checkbox" id="check" />
+//         <label htmlFor="check" className="checkbtn">
+//           <i className="fas fa-bars" />
+//         </label>
+//         <label className="logo">DesignX</label>
+//         <ul>
+//             <Link to='/home'>Home</Link>
         
-          <Link to='./flash'>Flashcard</Link>
-        </ul>
-      </nav>
-    )
-}
+//           <Link to='./flash'>Flashcard</Link>
+//         </ul>
+//       </nav>
+//     )
+// }
 
-export default Navbar;
+// export default Navbar;
